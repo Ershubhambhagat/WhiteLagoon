d9407b3e07f338acaab6611127fd11dda91d1190
Shubham Kumar <2090944@cognizant.com>
2024-01-30 18:29:37 +0530

    #Day2 #office Delete Villa is Compleated

--- WhiteLagoon.web/Controllers/VillaController.cs
+++ WhiteLagoon.web/Controllers/VillaController.cs
@@ -1,25 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
-
 namespace WhiteLagoon.web.Controllers
 {
-    
     public class VillaController : Controller
     {
+        #region Ctor
         private readonly ApplicationDbContext _db;
-
         public VillaController(ApplicationDbContext db)
         {
-            _db= db;
+            _db = db;
         }
+        #endregion
+
+        #region View Villa
+
         public IActionResult Index()
         {
-            var villa=_db.Villas.ToList();
+            var villa = _db.Villas.ToList();
             return View(villa);
         }
-      
-        
+        #endregion
+
+        #region Create Villa
         public IActionResult CreateVilla(Villa obj)
         {
             if (obj.Name == obj.Description)
@@ -28,7 +31,6 @@ namespace WhiteLagoon.web.Controllers
             }
             if (ModelState.IsValid)
             {
-
                 _db.Add(obj);
                 _db.SaveChanges();
                 TempData["success"] = "The villa has been created successfully.";
@@ -36,46 +38,55 @@ namespace WhiteLagoon.web.Controllers
             }
             return View();
         }
+        #endregion
 
-       
+        #region Update Villa
         public IActionResult Update(int villaId)
         {
-            Villa? obj = _db.Villas.FirstOrDefault(u=>u.Id == villaId);
-                
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
             if (obj == null)
             {
-                return RedirectToAction("Error","Home");
+                return RedirectToAction("Error", "Home");
             }
             return View(obj);
-
         }
         [HttpPost]
         public IActionResult Update(Villa obj)
         {
-          
             if (ModelState.IsValid && obj.Id > 0)
             {
-
                 _db.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             return View();
         }
+        #endregion
+
+        #region Delete Villa
 
 
-
-
-
-
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            _db.Villas.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
     }
-
-
-
-
-
-
-
-
-
 }

f5e536363058235a2e7c3ab924706af8f3168a76
Shubham Kumar <2090944@cognizant.com>
2024-01-31 12:48:57 +0530

    #Day3 #office Commit-5 Create and view Villa Number is Completed

--- WhiteLagoon.Domain/Entities/VillaNumber.cs
+++ WhiteLagoon.Domain/Entities/VillaNumber.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WhiteLagoon.Domain.Entities
@@ -7,11 +8,13 @@ namespace WhiteLagoon.Domain.Entities
     {
         //Primary key without  without default Identity Column
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display (Name ="Villa Number  ")]
         public int Villa_Number { get; set; }
 
         //Foreign Key Relation 
         [ForeignKey("Villa")]
         public int VillaId { get; set; }
+        [ValidateNever]
         public Villa villa { get; set; }
         public string? SpecialDetails { get; set; }
     }
---
+++ WhiteLagoon.web/Controllers/VillaNumberController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+namespace WhiteLagoon.web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        #region Ctor
+        private readonly ApplicationDbContext _db;
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region View Villa Number
+        public IActionResult Index()
+        {
+            var VillaNumber = _db.VillaNumber.ToList();
+            return View(VillaNumber);
+        }
+        #endregion
+
+        #region Create Villa Number
+        public IActionResult CreateVillaNumber()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateVillaNumber(VillaNumber obj)
+        {
+            ModelState.Remove("Villa");
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumber.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa Number  has been created successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        #endregion
+
+        #region Update Villa Number
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj == null)
+            {
+                TempData["error"] = "Failed to Update the villa.";
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        public IActionResult Update(VillaNumber obj)
+        {
+            if (ModelState.IsValid && obj.VillaId > 0)
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been Update successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        #endregion
+
+        #region Delete Villa Number
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj == null)
+            {
+                TempData["error"] = "Failed to delete the villa.";
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            if (obj == null)
+            {
+                TempData["error"] = "Failed to delete the villa.";
+                return RedirectToAction("Error", "Home");
+            }
+            _db.Villas.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "The villa has been Deleted successfully.";
+            return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}

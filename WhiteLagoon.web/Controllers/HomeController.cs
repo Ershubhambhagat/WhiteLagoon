d84ec38d9a9526797607f9d38caf2ec09ba150d8
Shubham Kumar <2090944@cognizant.com>
2024-02-21 18:46:48 +0530

    #Day3 #office Commit-4 Check available  Button
    If u choose back date buttun is not there and if its sold out its also disabled

--- WhiteLagoon.web/Controllers/HomeController.cs
+++ WhiteLagoon.web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 using WhiteLagoon.Application.Common.Interface;
 using WhiteLagoon.web.Models;
 using WhiteLagoon.web.Models.ViewModels;
@@ -8,26 +9,48 @@ namespace WhiteLagoon.web.Controllers
 {
     public class HomeController : Controller
     {
+        #region CTOR
         private readonly IUnitOfWork _unitOfWork;
+
         public HomeController(IUnitOfWork unitOfWork)
         {
-
             _unitOfWork = unitOfWork;
         }
+        #endregion
+
+        #region index
+
+
         public IActionResult Index()
         {
             HomeVM homeVM = new HomeVM()
             {
-                VillaList = _unitOfWork.Villa.GetAll(includeProperties : "VillaAmenity"),
+                VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity"),
                 CheckInDate = DateOnly.FromDateTime(DateTime.Now),
                 CheckOutDate = DateOnly.FromDateTime(DateTime.Now),
                 Nights = 1
-
-
             };
             return View(homeVM);
         }
 
+
+        [HttpPost]
+        public IActionResult Index(HomeVM homeVM)
+        {
+            homeVM.VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
+            foreach(var villa in  homeVM.VillaList)
+            {
+                if(villa.Id %2==0) 
+                {
+                    villa.IsAvailable = true;
+                }
+            }
+            return View(homeVM);
+               
+        }
+
+
+        #endregion
         public IActionResult Privacy()
         {
             return View();
